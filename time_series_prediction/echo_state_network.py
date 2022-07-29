15ec08e73480e333e2a2c78dd101b0924f9084f6
Sam Lishak <sam@lishak.com>
2022-07-29 10:06:58 +0100

    Docstrings: echo_state_network.py

--- time_series_prediction/echo_state_network.py
+++ time_series_prediction/echo_state_network.py
@@ -5,10 +5,9 @@ import numpy as np
 
 from time_series_prediction import settings
 
-# For details:
-# https://arxiv.org/pdf/2012.02974.pdf
 
 class ESN:
+    """Echo state network"""
     def __init__(
         self,
         w_in: float = 1, 
@@ -19,6 +18,21 @@ class ESN:
         n_inputs: int = 1,
         f_activation: Callable = torch.special.expit,
     ):
+        """Initialise echo state network.
+
+        See https://arxiv.org/pdf/2012.02974.pdf for more details.
+
+        Args:
+            w_in (float, optional): Input scaling parameter. Defaults to 1.
+            sparsity (float, optional): Sparsity of reservoir. Defaults to 0.1.
+            spectral_radius (float, optional): Spectral radius of reservoir. 
+                Defaults to 0.99.
+            n_neurons (int, optional): Size of reservoir. Defaults to 100.
+            n_outputs (int, optional): Number of ESN outputs. Defaults to 1.
+            n_inputs (int, optional): Number of ESN inputs. Defaults to 1.
+            f_activation (Callable, optional): Activation function. Defaults to
+                torch.special.expit.
+        """
         self.w_in = w_in
         self.sparsity = sparsity
         self.spectral_radius = spectral_radius
@@ -30,12 +44,16 @@ class ESN:
         self._generate()
 
     def _generate(self):
+        """Generate reservoir matrices/vectors based on the given parameters"""
         
         # For now, saple input/output/backprop layers from same distribution
         layer_weight_dist = torch.distributions.Uniform(-self.w_in, self.w_in)
-        self.input_weights = layer_weight_dist.sample([self.n_neurons, self.n_inputs]).to(settings.device)
-        self.backprop_weights = layer_weight_dist.sample([self.n_neurons, self.n_outputs]).to(settings.device)
-        self.output_weights = layer_weight_dist.sample([self.n_outputs, self.n_neurons]).to(settings.device)
+        self.input_weights = layer_weight_dist.sample(
+            [self.n_neurons, self.n_inputs]).to(settings.device)
+        self.backprop_weights = layer_weight_dist.sample(
+            [self.n_neurons, self.n_outputs]).to(settings.device)
+        self.output_weights = layer_weight_dist.sample(
+            [self.n_outputs, self.n_neurons]).to(settings.device)
 
         # Reservoir
         sparsity_dist = torch.distributions.Bernoulli(self.sparsity)
@@ -52,13 +70,25 @@ class ESN:
         self, 
         inp: torch.Tensor, 
         prev_state: torch.Tensor, 
-        prev_obs: torch.Tensor
+        prev_output: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Compute next state and output given the current state and output.
+
+        Args:
+            inp (torch.Tensor): Next input
+            prev_state (torch.Tensor): Current state
+            prev_output (torch.Tensor): Current output
+
+        Returns:
+            Tuple containing:
+            - torch.Tensor: next state
+            - torch.Tensor: next (predicted) output
+        """
 
         x = self.f_activation(
             self.input_weights @ inp
             + self.w @ prev_state
-            + self.backprop_weights @ prev_obs
+            + self.backprop_weights @ prev_output
             )
 
         y = x @ self.output_weights
@@ -67,17 +97,30 @@ class ESN:
     def predict(
         self, 
         u: torch.Tensor, 
-        x_i: torch.Tensor, 
-        y_i: torch.Tensor
+        x_0: torch.Tensor, 
+        y_0: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Run the forwards function repeatedly and output a sequence of states
+        and outputs
+
+        Args:
+            u (torch.Tensor): Inputs to model. If the model has no inputs 
+                (self.n_inputs=0), then this must be a tensor of shape [n*0]
+                where n is the length of the required output sequence
+            x_0 (torch.Tensor): Initial reservoir state of shape 
+                [self.n_neurons]
+            y_0 (torch.Tensor): Initial output of shape [self.n_outputs]
+
+        Returns:
+            Tuple containing:
+            - torch.Tensor: ESN states
+            - torch.Tensor: ESN outputs
+        """
         x_esn = []
         y_esn = []
+        x_i = x_0
+        y_i = y_0
 
-        # x_dist = torch.distributions.Uniform(-self.w_in, self.w_in)
-        # x_i = x_dist.sample([self.n_neurons]) / 100
-
-        # x_i = torch.zeros((self.n_neurons))
-        # y_i = torch.zeros([self.n_outputs])
         for i in range(u.shape[0]):
             x_i, y_i = self.forwards(u[i, :], x_i, y_i)
             x_esn.append(x_i.detach().cpu().numpy())
@@ -87,32 +130,55 @@ class ESN:
 
     def train(
         self, 
-        inp: torch.Tensor, 
-        obs: torch.Tensor, 
+        inputs: torch.Tensor, 
+        outputs: torch.Tensor, 
         n_discard: int = 100, 
         k_l2: float = 0,
     ) -> torch.Tensor:
-        
-        n_steps = obs.shape[0]
+        """Generate output layer weights by driving the network with a sequence
+        of inputs and outputs to a process, and performing ridge (L2) 
+        regression on the reservoir states such that the ESN outputs 
+        approximate the process outputs.
 
-        # Initialise with zero weights
+        Args:
+            inputs (torch.Tensor): Inputs to process, of shape 
+                [n*self.n_inputs]. Note that the first input is not used in
+                training as the reservoir is initialised with zeros. If there
+                are no inputs this must still be of shape [n*0] 
+            outputs (torch.Tensor): Outputs of process, of shape 
+                [n*self.n_outputs]
+            n_discard (int, optional): Number of initial reservoir states to 
+                discard when training the output layer. Used to make sure the
+                arbitrary choice of initial conditions does not affect the
+                solution. Consider analogous to MCMC burn-in. Defaults to 100.
+            k_l2 (float, optional): L2 regression strength. Defaults to 0.
+
+        Returns:
+            torch.Tensor: Reservoir states driven by the inputs and outputs, of
+                shape [n*self.n_neurons]
+        """
+        
+        n_steps = outputs.shape[0]
+
+        # Initialise with zeros
         x = torch.zeros((n_steps, self.n_neurons), device=settings.device)
 
         # Drive network with training data
         for i in range(1, n_steps):
             x[i, :] = self.f_activation(
-                self.input_weights @ inp[i, :]
+                self.input_weights @ inputs[i, :]
                 + self.w @ x[i-1, :]
-                + self.backprop_weights @ obs[i-1, :]
+                + self.backprop_weights @ outputs[i-1, :]
             )
 
-        phi = torch.concat((x, inp), 1)[n_discard:, :]
-        y = obs[n_discard:, :]
-        #self.output_weights = torch.pinverse(phi[n_discard:, :]) @ torch.tensor(obs[n_discard:, :])
+        # Ridge regression: minimise ||y - phi * w||^2_2 +- alpha * ||w||^2_2
+        phi = torch.concat((x, inputs), 1)[n_discard:, :]
+        y = outputs[n_discard:, :]
 
         self.output_weights = torch.linalg.solve(
             torch.matmul(phi.T, phi) + k_l2 * torch.eye(self.n_neurons, device=settings.device), 
             torch.matmul(phi.T, y))
+        # self.output_weights = torch.pinverse(phi[n_discard:, :]) @ torch.tensor(outputs[n_discard:, :])
         # self.output_weights, res, rank, sing = torch.linalg.lstsq(phi, y)
 
         return x
